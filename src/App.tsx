5ed87ba45fa06560b5446fa5435bfec6ec76543c
remogh81-ship-it <remogh81@gmail.com>
2026-09-13 10:33:11 +0300

    Update import path for GrowthRateCalculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import GrowthRateCalculator from './components/GrowthRateCalculator';
+import GrowthRateCalculator from 'src/components/GrowthRateCalculator';
 import { useEffect, useState } from 'react';
 import { Plus } from 'lucide-react';
 import { AdSlot } from './components/AdSlot';
